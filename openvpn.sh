5bdf90d78590e16253581da97038743c292c5576
Ignatius Teo <iggy@codeworx.com.au>
2020-05-27 12:35:42 +1000

    Fix to use mounted auth_conf

--- openvpn.sh
+++ openvpn.sh
@@ -5,11 +5,16 @@ if [ -n "$REGION" ]; then
   set -- "$@" '--config' "${REGION}.ovpn"
 fi
 
+touch /run/secrets/auth_conf
+
 if [ -n "$USERNAME" -a -n "$PASSWORD" ]; then
-  echo "$USERNAME" > auth.conf
-  echo "$PASSWORD" >> auth.conf
-  chmod 600 auth.conf
-  set -- "$@" '--auth-user-pass' 'auth.conf'
+  echo "$USERNAME" > /run/secrets/auth_conf
+  echo "$PASSWORD" >> /run/secrets/auth_conf
 fi
 
+chmod 600 /run/secrets/auth_conf
+set -- "$@" '--auth-nocache'
+set -- "$@" '--auth-user-pass' '/run/secrets/auth_conf'
+
+echo Starting OpenVPN with "$@"
 openvpn "$@"
